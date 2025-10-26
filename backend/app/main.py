2dfd84c9162080151a541a3a83bfe4a52295e7af
kikorin72 <kritigupta2407@gmail.com>
2025-10-26 19:08:56 +0530

    setup database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app.db.database import engine, SessionLocal, Base
+from sqlalchemy import text
 
 app = FastAPI()
 
+# Dependency for DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root(db: Session = Depends(get_db)):
+    try:
+        db.execute(text("SELECT 1"))  # simple test query
+        return {"message": "Database connection successful!"}
+    except Exception as e:
+        return {"error": str(e)}
